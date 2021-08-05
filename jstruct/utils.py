830981635363c311a89aa416bff62afc4237b703
DanH91 <danielk.developer@gmail.com>
2021-08-05 03:23:27 -0400

    handle issue with type without annotation

--- jstruct/utils.py
+++ jstruct/utils.py
@@ -5,8 +5,9 @@ logger = logging.getLogger(__name__)
 
 
 def instantiate(class_: typing.Type, args: dict):
-    supported_args = {key: val for key, val in args.items() if key in class_.__annotations__}
-    unsupported_args = {key: val for key, val in args.items() if key not in class_.__annotations__}
+    annotations = getattr(class_, '__annotations__', {})
+    supported_args = {key: val for key, val in args.items() if key in annotations}
+    unsupported_args = {key: val for key, val in args.items() if key not in annotations}
 
     if any(unsupported_args.items()):
         logger.warning(f"unknown arguments {unsupported_args}")
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
       name='jstruct',
-      version='2021.7',
+      version='2021.7.1',
       description='Readable serializable and deserializable Python nested models',
       long_description=long_description,
       long_description_content_type="text/markdown",
